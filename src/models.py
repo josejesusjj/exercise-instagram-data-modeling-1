de36fb6f03b1d6fc3010039b5efdc6e9ae323a71
Jose <josejesuscedeno@gmail.com>
2022-03-04 11:01:01 +0000

    ready to publish

--- src/models.py
+++ src/models.py
@@ -8,23 +8,67 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250), nullable=False)
+    phone_number = Column(String(25))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(5000), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    user = relationship(User)
+
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table address.
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    description = Column(String(500))
+    tags = Column(String(2500))
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
+
+class Media(Base):
+    __tablename__ = 'Media'
+    # Here we define columns for the table address.
+    id = Column(Integer, primary_key=True)
+    media_type = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    user = relationship(User)
+
+class Tag(Base):
+    __tablename__ = 'tag'
+    # Here we define columns for the table address.
+    id = Column(Integer, primary_key=True)
+    tag_name = Column(String(250), nullable=False)
+    user = relationship(User)
+
+class Post_tag(Base):
+    __tablename__ = 'post_tag'
+    # Here we define columns for the table address.
+    id = Column(Integer, primary_key=True)
+    tag_from_id = Column(Integer, ForeignKey('tag.id'), nullable=False)
+    post_to_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    user = relationship(User)
 
     def to_dict(self):
         return {}
